3cd97eaeca705731985324ac0247b63dd0566675
RatanaSenpai <createbysenpai@gmail.com>
2024-02-06 15:46:12 +0100

    💄 HEADER > Add logo + subtitle + input

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ export function App() {
     return (
 
         <div className={s.main_container}>
-            <div className={s.header}>Header</div>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                        <input style={{ width: "100%" }} type="text" />
+                    </div>
+                </div>
+            </div>
             <div className={s.tv_show_detail}>Detail</div>
             <div className={s.recommendations}>Recommendations</div>
         </div>
