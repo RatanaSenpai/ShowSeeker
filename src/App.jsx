a075404fb57c1f8de1f1b396a6be0c1abb0c571d
RatanaSenpai <createbysenpai@gmail.com>
2024-02-06 15:22:32 +0100

    💄  LAYOUT > Add Header + Detail + Recommendations

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,12 @@ import s from "./style.module.css"
 
 export function App() {
 
-    return <div>Ratana</div>
+    return (
+
+        <div className={s.main_container}>
+            <div className={s.header}>Header</div>
+            <div className={s.tv_show_detail}>Detail</div>
+            <div className={s.recommendations}>Recommendations</div>
+        </div>
+    )
 }
