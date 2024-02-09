0fcf0fa880d88a82bfa5a5258cb8ae9b643f0bd6
RatanaSenpai <createbysenpai@gmail.com>
2024-02-09 19:22:43 +0100

    💄 TVShowListItem > Change size cover

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_PARAM = "?api_key=9adccddab05670b7227438f18baa5f60";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
-const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w400/";
 
 export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL }
