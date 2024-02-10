f9fff65d738677b9c4fb8a8366d9a93626e47ed9
RatanaSenpai <createbysenpai@gmail.com>
2024-02-10 14:44:58 +0100

    🙈 Add /.env to gitignore and create new environment

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,22 +1,22 @@
 import axios from "axios";
-import { BASE_URL, API_KEY_PARAM } from "../config";
+import { BASE_URL } from "../config";
 
 export class TVShowAPI {
     static async fetchPopulars() {
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}&language=fr-FR`);
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}&language=fr-FR`);
         return response.data.results;
     }
 
     static async fetchRecommendations(tvShowId) {
         const response = await axios.get(
-            `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}&language=fr-FR`
+            `${BASE_URL}tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}&language=fr-FR`
         );
         return response.data.results;
     }
 
     static async fetchByTitle(title) {
         const response = await axios.get(
-            `${BASE_URL}search/tv${API_KEY_PARAM}&language=fr-FR&query=${title}`
+            `${BASE_URL}search/tv$?api_key=${process.env.REACT_APP_API_KEY_PARAM}&language=fr-FR&query=${title}`
         );
         return response.data.results;
     }
